ef40f501e3a5dc0664c2122e7a1a31915cff9b57
ErikBlomsterberg <erik.blomsterberg@gmail.com>
2023-04-03 15:10:25 +0200

    meddelande

--- Project/App.js
+++ Project/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! KAROLINA TESTAR</Text>
+      <Text>Open up App.js to start working on your app! ERIK TESTAR</Text>
       <StatusBar style="auto" />
     </View>
   );
