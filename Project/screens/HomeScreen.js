df42a921ada506f115aeae7cc8f1a7e9094201bc
Karolina Hagerman <hagermankarolina@gmail.com>
2023-05-11 12:47:33 +0200

    tryout design on homescreen, only temporary font

--- Project/screens/HomeScreen.js
+++ Project/screens/HomeScreen.js
@@ -1,36 +1,89 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Button, View} from 'react-native';
-import {useState} from 'react';
+import { StyleSheet, Button, View, Image, TouchableOpacity, Text } from 'react-native';
+import { useState } from 'react';
 import eng from '../languages/eng.json';
 import sve from '../languages/sve.json';
+import { Ionicons } from '@expo/vector-icons';
+import { useFonts } from 'expo-font';
+import AppLoading from 'expo-app-loading';
 
 
-//import eng from '../languages/eng.json';
-//import sve from '../languages/sve.json';
+const ICON_SIZE = 40;
 
 export default function HomeScreen({ navigation }) {
   const [language, setLanguage] = useState(eng);
-  return (   
-    <View style={styles.container}>
-      <Button 
-        title= "Svenska"
-        onPress={() => {
-          setLanguage(sve);
-        }
-        }
-      />
-      <Button 
-        title= "English"
-        onPress={() => {
-          setLanguage(eng);
-        }
-        }
-      />
+  const [soundOn, setSound] = useState(false)
 
-      <Button 
-        title= {language.HomeScreen.newGameButton}
-        onPress={() => navigation.navigate("NewGame", {language: language})}
-      />
+  // Fonts, download from https://www.dafont.com/ and add to fonts folder
+  let [fontsLoaded] = useFonts({
+    'impact': require('../fonts/impact.ttf'),
+    'swallowKick': require('../fonts/SwallowKick.ttf')
+  });
+
+  if(!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <View style={styles.flags}>
+          <TouchableOpacity
+            style={styles.headerItem}
+            onPress={() => {
+              setLanguage(eng);
+            }
+            }
+          >
+            <Image
+              source={require('../images/eng.png')}
+              style={[styles.flag, language === eng && styles.chosenFlag]}
+            >
+            </Image>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.headerItem}
+            onPress={() => {
+              setLanguage(sve);
+            }
+            }
+          >
+            <Image
+              source={require('../images/swe.png')}
+              style={[styles.flag, language === sve && styles.chosenFlag]}
+            >
+            </Image>
+          </TouchableOpacity>
+
+        </View>
+
+
+        <TouchableOpacity
+          style={[styles.headerItem, styles.soundIcon]}
+          onPress={() => setSound(!soundOn)}
+        >
+          {soundOn ? (
+            <Ionicons name="volume-high-outline" size={ICON_SIZE} color="black" />
+          ) : (
+            <Ionicons name="volume-mute-outline" size={ICON_SIZE} color="black" />
+          )}
+        </TouchableOpacity>
+
+      </View>
+
+      <View style={styles.body}>
+
+        {/*       <Image style={styles.image} source={require('../assets/BILD')}/> */}
+
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.navigate("NewGame", { language: language, soundOn: soundOn })}
+        >
+          <Text style={styles.buttonText}>{language.HomeScreen.newGameButton}</Text>
+        </TouchableOpacity>
+      </View>
+
 
       <StatusBar style="auto" />
     </View>
@@ -44,4 +97,55 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    flexDirection: 'row',
+
+    alignSelf: 'stretch',
+    justifyContent: 'space-between',
+    padding: '2%',
+    marginLeft: '5%',
+    marginRight: '5%',
+    marginTop: '10%',
+  },
+  body: {
+    marginTop: '60%',
+    flex: 1,
+  },
+  flags: {
+    flexDirection: 'row',
+  },
+  headerItem: {
+    //INKLUDERAR FLAGGOR OCH LJUDSYMBOL, 
+  },
+  flag: {
+    width: 60,
+    height: ICON_SIZE,
+    marginRight: '3%',
+  },
+  chosenFlag: {
+    borderColor: 'red',
+    borderWidth: 2,
+  },
+  soundIcon: {
+    //här kan vi lägga till
+  },
+  image: {
+    //HÄR LÄGGER VI TILL FÖR BILDEN
+  },
+  button: {
+    borderColor: 'black',
+    backgroundColor: 'white',
+    borderWidth: 2,
+    borderRadius: 15,
+    color: 'black',
+    padding: 10,
+    shadowColor: 'black',
+    shadowOffset: { width: -2, height: 4 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+  },
+  buttonText: {
+    fontSize: 40,
+    fontFamily: 'swallowKick',
+  }
 });
